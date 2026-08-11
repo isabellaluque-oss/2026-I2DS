8f18197460f3a8b37ef0520ac33577e45a5d3332
isabellaluque-oss <isabella.luque@edu.senai.br>
2026-08-11 08:31:42 -0300

    Aula04_ByIsabella

--- Aula-04/03-Encapsulamento/Encapsulamento/obj/Debug/net10.0/Encapsulamento.AssemblyInfo.cs
+++ Aula-04/03-Encapsulamento/Encapsulamento/obj/Debug/net10.0/Encapsulamento.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Encapsulamento")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2395088c7e389d84ba507381c418978457a86d84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90557ce2b0a394612c0bfaa5f9cc2ae6428a6b80")]
 [assembly: System.Reflection.AssemblyProductAttribute("Encapsulamento")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Encapsulamento")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
